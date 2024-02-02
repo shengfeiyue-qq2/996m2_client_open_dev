eb851da45bf95682ffd4a05b4cea422b1fa55b2f
wawa <Joy_summer_7@yeah.net>
2024-02-02 14:15:01 +0800

    + GUIExport GUILayout

---
+++ GUILayout/HeroEquip_Look_win32.lua
@@ -0,0 +1,178 @@
+-- 查看他人面板 装备
+HeroEquip_Look = {}
+
+HeroEquip_Look._ui = nil
+
+-- 13 斗笠位置比较特殊 属于和头盔位置同部位
+-- 要斗笠和头盔分开 需要设置Panel_pos13为显示 
+HeroEquip_Look.showModelCapAndHelmet = false -- 斗笠和头盔分开情况下  模型是否显示 斗笠头盔
+HeroEquip_Look._hideNodePos = {}
+HeroEquip_Look.RoleType = {
+    OtherHero = 11 -- 查看他人
+}
+function HeroEquip_Look.main(data)
+    HeroEquip_Look.posSetting = {
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 16
+    }
+    local parent = GUI:Attach_Parent()
+    GUI:LoadExport(parent, "hero_look/hero_equip_node_win32")
+
+    HeroEquip_Look._ui = GUI:ui_delegate(parent)
+    if not HeroEquip_Look._ui then
+        return false
+    end
+    HeroEquip_Look._parent = parent
+
+    -- 初始化装备槽
+    HeroEquip_Look.InitEquipCells()
+    -- 角色性别
+    HeroEquip_Look.playerSex = SL:GetMetaValue("L.M.SEX")
+    -- 发型
+    HeroEquip_Look.playerHairID = SL:GetMetaValue("L.M.HAIR")
+    -- 职业
+    HeroEquip_Look.playerJob = SL:GetMetaValue("L.M.JOB")
+    
+
+    -- 首饰盒
+    local ringBoxShow = SL:GetMetaValue("SERVER_OPTION", SW_KEY_SNDAITEMBOX) == 1 -- 首饰盒功能是否开启
+    GUI:setVisible(HeroEquip_Look._ui.Best_ringBox, ringBoxShow)
+    GUI:addOnClickEvent(HeroEquip_Look._ui.Best_ringBox,function()
+        --首饰盒是否开启
+        local activeState = SL:GetMetaValue("BEST_RING_OPENSTATE", HeroEquip_Look.RoleType.OtherHero)
+        if activeState then
+            SL:OpenBestRingBoxUI(HeroEquip_Look.RoleType.OtherHero, { param = {} })
+        else
+            --提示
+            local bestRingsName = SL:GetMetaValue("SERVER_OPTION", "SndaItemBoxName") or "首饰盒"
+            local worldPos = GUI:getTouchEndPosition(HeroEquip_Look._ui.Best_ringBox)
+            GUI:ShowWorldTips(string.format("%s未开启", bestRingsName), worldPos, GUI:p(0, 1))
+        end
+    end)
+    GUI:Text_setString(HeroEquip_Look._ui.Text_guildinfo, "")
+    --刷新首饰盒状态
+    HeroEquip_Look.RefreshPlayerBestRingsOpenState()
+    HeroEquip_Look.RefreshBestRingBox()
+    ----------------------
+    HeroEquip_Look.RegisterEvent()
+end
+
+function HeroEquip_Look.InitHideNodePos()
+    HeroEquip_Look._hideNodePos = {}
+    local posList = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 55}
+    for _, i in ipairs(posList) do
+        if HeroEquip_Look._ui[string.format("Node_%s", i)] then
+            local visible = GUI:getVisible(HeroEquip_Look._ui[string.format("Node_%s", i)])
+            if not visible then
+                HeroEquip_Look._hideNodePos[i] = true
+            end
+        end
+    end
+end
+
+function HeroEquip_Look.InitEquipCells()
+    local uid = SL:GetMetaValue("LOOK_USER_ID")
+
+    --请求通知脚本查看uid的珍宝
+    SL:RequestLookZhenBao(uid)
+    -- 额外的装备位置
+    local equipPosSet = SL:GetMetaValue("SERVER_OPTION", SW_KEY_EQUIP_EXTRA_POS) or 0
+    local showExtra = equipPosSet == 1
+    if showExtra then
+        table.insert(HeroEquip_Look.posSetting, 14)
+        table.insert(HeroEquip_Look.posSetting, 15)
+    else
+        GUI:setVisible(HeroEquip_Look._ui.Panel_pos14, false)
+        GUI:setVisible(HeroEquip_Look._ui.Panel_pos15, false)
+        GUI:setVisible(HeroEquip_Look._ui.Node_14, false)
+        GUI:setVisible(HeroEquip_Look._ui.Node_15, false)
+    end
+end
+
+function HeroEquip_Look.RefreshPlayerBestRingsOpenState(data)
+    local activeState = SL:GetMetaValue("BEST_RING_OPENSTATE", HeroEquip_Look.RoleType.OtherHero)
+    if activeState then
+        GUI:Image_setGrey(HeroEquip_Look._ui.Image_box, false)
+    else
+        GUI:Image_setGrey(HeroEquip_Look._ui.Image_box, true)
+    end
+    
+    local function mouseMoveCallBack(touchPos)
+        if not GUI:getVisible(HeroEquip_Look._ui.Best_ringBox) then 
+            return
+        end
+        local bestRingsName = SL:GetMetaValue("SERVER_OPTION", "SndaItemBoxName") or "首饰盒"
+        if SL:CheckNodeCanCallBack(HeroEquip_Look._ui.Best_ringBox, touchPos) and bestRingsName ~= "" then
+            local activeState = SL:GetMetaValue("BEST_RING_OPENSTATE", HeroEquip_Look.RoleType.OtherHero)
+            local playerName = SL:GetMetaValue("LOOK_USER_NAME")
+            local tipsStr = ""
+            if activeState then
+                tipsStr = string.format("点击打开%s",bestRingsName)
+            else
+                tipsStr = string.format("%s的%s未开启",playerName,bestRingsName)
+            end
+            local worldPos = GUI:getWorldPosition(HeroEquip_Look._ui.Best_ringBox)
+            worldPos.x = GUI:getContentSize(HeroEquip_Look._ui.Best_ringBox).width / 2 + worldPos.x
+            GUI:ShowWorldTips(tipsStr, worldPos, GUI:p(0.5, 1))
+        end
+    end
+
+    local function leaveItem()
+        local nodeTips = GUI:getChildByName(HeroEquip_Look._ui.Best_ringBox,"TIPS")
+        if nodeTips then
+            GUI:removeFromParent(nodeTips)
+            nodeTips = nil
+        end
+    end
+
+    GUI:addMouseMoveEvent(HeroEquip_Look._ui.Best_ringBox,
+        {
+            onEnterFunc = mouseMoveCallBack,
+            onLeaveFunc = leaveItem
+        }
+    )
+    if data and data.isOpen then
+        if activeState then
+            SL:OpenBestRingBoxUI(HeroEquip_Look.RoleType.OtherHero, { param = {} })
+        end
+    end
+end
+
+function HeroEquip_Look.RefreshBestRingBox()
+    SL:scheduleOnce(HeroEquip_Look._ui.Best_ringBox,function()
+        local texture = "btn_jewelry_1_1.png"
+        if SL:GetMetaValue("BEST_RING_WIN_ISOPEN", HeroEquip_Look.RoleType.OtherHero) then -- 首饰盒界面是否打开
+            texture = "btn_jewelry_1_0.png"
+        end
+        GUI:Image_loadTexture(HeroEquip_Look._ui.Image_box, SLDefine.PATH_RES_PRIVATE .. "player_best_rings_ui/player_best_rings_ui_win32/" .. texture)
+        -- 重置尺寸
+        GUI:setIgnoreContentAdaptWithSize(HeroEquip_Look._ui.Image_box, true)
+        HeroEquip_Look.RefreshPlayerBestRingsOpenState()
+    end,0.1)
+end
+
+--[[    
+    创建装备回调
+    item 装备
+    返回 item
+]]
+function HeroEquip_Look.CreateEquipItemCallBack(item)
+    return item
+end
+--[[    
+    创建人物模型回调
+]]
+function HeroEquip_Look.CreateModelCallBack(model)
+    return model
+end
+--[[    
+    界面关闭回调
+]]
+function HeroEquip_Look.CloseCallback()
+    HeroEquip_Look.UnRegisterEvent()
+end
+
+function HeroEquip_Look.RegisterEvent()
+end
+
+function HeroEquip_Look.UnRegisterEvent()
+end
